91134fcbff3a610df7d1632922ab106f4ee0348e
ghulammuhammad84 <gm.84@live.com>
2021-11-09 21:18:11 +1100

    Api calling work completed in weatherApp (1st class with Tariq)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+  const [weatherState, setWeatherState] = useState([]);
+  useEffect(() => {
+    async function FetchApi() {
+      let weatherApi = await fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json');
+      let jsonApi = await weatherApi.json();
+      setWeatherState(jsonApi.dataseries);
+      }
+    FetchApi();
+  }, []);
+    console.log(weatherState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
 
-export default App;
+    </div>
+  )
+}
+export default App
